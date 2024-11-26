967bff030668b51f41220279dee8b9a42d395cc2
Zephuros <rockman_after_legend@hotmail.com>
2024-11-26 22:00:11 +0700

    main menu
    1

---
+++ UI/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _on_play_pressed() -> void:
+	get_tree().pass()
+
+func _on_how_to_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_fullscreen_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
