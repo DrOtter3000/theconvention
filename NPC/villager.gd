48259eaf079d39bb409db60076ae99cbdf6967e3
DrOtter3000 <info@darknet-online.com>
2024-11-27 16:23:25 +0100

    make villager move automatic

--- NPC/villager.gd
+++ NPC/villager.gd
@@ -1,46 +1,45 @@
 extends CharacterBody3D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+var speed = 4.9
 
-@export var speed = 3.0
 
-@export var available_positions: Array
 var target_position = Vector3.ZERO
+var list_of_target_positions = []
+
+@onready var nav: NavigationAgent3D = $NavigationAgent3D
+
+var in_line_of_sight : bool
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
-var movement_speed: float = 2.0
-var movement_target_position: Vector3 = Vector3.ZERO
+func _ready():
+	randomize()
+	list_of_target_positions = get_node("../TargetPositions").get_children()
+	set_new_target_position()
 
-func _ready() -> void:
-	movement_target_position = available_positions.pick_random()
-	# These values need to be adjusted for the actor's speed
-	# and the navigation layout.
-	navigation_agent.path_desired_distance = 0.5
-	navigation_agent.target_desired_distance = 0.5
 
-	# Make sure to not await during _ready.
-	call_deferred("actor_setup")
-
-func actor_setup():
-	# Wait for the first physics frame so the NavigationServer can sync.
-	await get_tree().physics_frame
-
-	# Now that the navigation map is no longer empty, set the movement target.
-	set_movement_target(movement_target_position)
-
-func set_movement_target(movement_target: Vector3):
-	navigation_agent.set_target_position(movement_target)
-
-func _physics_process(delta):
-	if navigation_agent.is_navigation_finished():
-		return
-
-	var current_agent_position: Vector3 = global_position
-	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
+func _process(delta):
+	speed = 2
+	var direction = Vector3()
+			
+	nav.target_position = target_position.global_position
 	
-	look_at(next_path_position)
+	direction = nav.get_next_path_position() - global_position
+	direction.y = 0
+	direction = direction.normalized()
 	
-	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	velocity = direction * (speed)
+	velocity.y -= gravity * delta
+	
+	var look_direction = -Vector2(velocity.z, velocity.x)
+	$Villager.rotation.y = look_direction.angle()
+	
+	if nav.is_navigation_finished():
+		set_new_target_position()
+
+	velocity.y -= gravity * delta
 	move_and_slide()
+
+
+func set_new_target_position():
+	target_position = list_of_target_positions.pick_random()
